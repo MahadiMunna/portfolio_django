2cb3a164f93f458d19db061ef55f72509cd3f6b1
MahadiMunna <mahadimunna.official@gmail.com>
2022-11-07 13:31:46 +0600

    admin, sql

--- new/models.py
+++ new/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name = models.CharField(max_length=50,null=True)
+    email=models.EmailField(max_length=60,null=True)
+    message=models.TextField(max_length=500,null=True)
+
